e3ea3a5c6da60e77a8789f71a9c580878f53e7a3
Jasf <bazoocaze@gmail.com>
2026-08-12 22:55:25 -0300

    feat: bump my-java-app1 hello message to v2

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World from my-java-app1 v1";
+        return "Hello World from my-java-app1 v2";
     }
 }
